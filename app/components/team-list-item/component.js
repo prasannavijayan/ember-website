4a4a20680cbc6f1cfaf1b98e6f699b849fde890b
ghislaineguerin <me@ghislaineguerin.com>
2019-09-12 16:53:23 +0200

    add layout classes to team page

--- app/components/team-list-item/component.js
+++ app/components/team-list-item/component.js
@@ -1,6 +1,6 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNames: ['list-team-item'],
-  tagName: 'li'
+  classNames: ['col-1-large'],
+  tagName: 'div'
 });
